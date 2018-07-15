b5b75ad2ca57feac0fb22601a3fa28708b7ab95f
victorlbauer <victor_lb80@hotmail.com>
2018-07-15 19:08:04 -0300

    corrigindo algumas partes

--- trabAD.py
+++ trabAD.py
@@ -6,6 +6,28 @@ import simpy
 # TODO: terminar as implementações com os tempos corretos
 #       calcular os dados finais
 
+class VoicePacket(object):
+    def __init__(self, env, server, name, i):
+        self.env = env
+        self.server = server
+        self.name = name
+        self.i = i
+        
+        self.action = env.process(self.run())
+        
+    def run(self):
+        while True:
+            with self.server.request() as req:
+                yield req
+                print ('%s[%d] sendo processado' % (self.name, self.i))
+                yield self.env.process(self.process(100))
+                print ('%s[%d] partiu. T = %d ms' % (self.name, self.i, env.now))
+            break
+        
+    def process(self, duration):
+        yield self.env.timeout(duration)
+        
+    
 class Voice(object):
     def __init__(self, env, server, name, prio):
         self.env = env
@@ -13,8 +35,9 @@ class Voice(object):
         self.name = name
         self.prio = prio
         
-        self.mean_silence = 755.0
+        self.mean_silence = 650.0
         self.mean_packet = 22.0
+        self.packet_rate = 16
         
         self.action = env.process(self.run())
         
@@ -24,31 +47,21 @@ class Voice(object):
         yield self.env.process(self.process(t))
         
         while True:
-            # Chegando na fila
-            print ('%s chegando na fila no tempo %d ms' % (self.name, env.now))
+            # Acordando o processo
+            print ('%s acordou. T = %d ms' % (self.name, env.now))
+            
+            # Enviando pacotes para a fila
+            print ('%s começou a enviar pacotes para a fila. T = %d ms' % (self.name, env.now))
+            for i in xrange(0, 3):
+                yield self.env.process(self.process(self.packet_rate))
+                print ('%s[%d] entrou na fila. T = %d ms' % (self.name, i, env.now))
+                packet = VoicePacket(self.env, self.server, self.name, i)
                 
-            # Entra na fila (com prioridade)
-            req = self.server.request(priority = self.prio)
-            yield req
-                
-            # Assim que chega sua vez, começa a ser processado
-            print ('%s sendo processado no tempo %d ms' % (self.name, env.now))
-            yield self.env.process(self.process(500))
-            server.release(req)
-            print ('%s partiu no tempo %d ms' % (self.name, env.now))
-                
-            # Fica em silencio por um periodo e depois volta a ativadade
+    
             t = self.silence()
             print ('%s tempo de silencio: %d ms' % (self.name, t))
             yield self.env.process(self.process(t))
-                
-            # Teste para finalizar a simulação
-            # TODO: critério de parada
-            '''
-            if(env.now >= 3000):
-                print ("Terminando")
-                break
-            '''
+    
         
     # Gera um periodo de silencio
     def silence(self):
@@ -61,7 +74,7 @@ class Voice(object):
         # p = 1/22, q = 1 - 1/22 =~ 0.954545
         q = 1.0 - (1.0/self.mean_packet)
         prob = random.uniform(0, 1)
-        return math.ceil(math.log(prob)/math.log(q))
+        return int(math.ceil(math.log(prob)/math.log(q)))
             
     def process(self, duration):
         yield self.env.timeout(duration)
@@ -79,20 +92,25 @@ class Data(object):
         self.superior_limit = 0.61337
         self.angle = 0.000208914
         
-        self.data_packet = [64, 512, 1500]
+        self.data_size = [64, 512, 1500]
+        self.twoMB = 2097152.0 # 2MB = 2.097.152 bytes
         
         self.action = env.process(self.run())
         
     def run(self):
         while True:
             try:
+                # Taxa de chegada = 10% de utilização
+                yield env.timeout(100)
+                
                 # Entra na fila (com prioridade)
                 req = self.server.request(priority = self.prio)
                 yield req
                 
                 # Assim que chega sua vez, começa a ser processado
-                print ('%s sendo processado no tempo %d ms' % (self.name, env.now))
-                yield self.env.process(self.process(300))
+                t_processo = self.data_packet()/self.twoMB
+                print ('Sendo processado no tempo %d ms. Tempo de serviço = %f' % (env.now, t_processo))
+                yield self.env.process(self.process(t_processo))
                 server.release(req)
                 
             except simpy.Interrupt as interrupt:
@@ -106,21 +124,21 @@ class Data(object):
         x = prob - self.inferior_limit
         
         if(prob < self.inferior_limit):
-            return self.data_packet[0]
+            return self.data_size[0]
         if(prob >= self.inferior_limit and prob < .4):
             return math.ceil((x)/self.angle)
         if(prob >= .4 and prob < .5):
-            return self.data_packet[1]
+            return self.data_size[1]
         if(prob >= .5 and prob < self.superior_limit + .1):
             return math.ceil((x - .1)/self.angle)
         else:
-            return self.data_packet[2]
+            return self.data_size[2]
         
     def process(self, duration):
         yield self.env.timeout(duration)
 
 if __name__ == "__main__":
-    n_voz = 1
+    n_voz = 2
     env = simpy.Environment()
     preemptive = True
     
@@ -133,8 +151,9 @@ if __name__ == "__main__":
     for i in xrange(0, n_voz):    
         voice = Voice(env, server, 'Voice[%s]' % i, 0)
          
+    #q = Queue(env, server, queue)
     # Inicializa o cliente de dados    
-    data = Data(env, server, 'Data', 1)
+    #data = Data(env, server, 'Data', 1)
     
     # Roda a simulação
-    env.run(until=3000)
+    env.run(until=2000)
